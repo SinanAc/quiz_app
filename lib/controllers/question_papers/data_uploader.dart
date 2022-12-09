5a5ad62d1608a6b46823e61931bb1dc493813b99
Sinan <sinanac124@gmail.com>
2022-12-09 11:31:20 +0530

    data upload

--- lib/controllers/question_papers/data_uploader.dart
+++ lib/controllers/question_papers/data_uploader.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:quiz_app/firebase_ref/loading_status.dart';
 import 'package:quiz_app/firebase_ref/references.dart';
 import 'package:quiz_app/models/question_ppr_model.dart';
 
@@ -12,13 +13,15 @@ class DataUploader extends GetxController {
     uploadData();
     super.onReady();
   }
+  final loadingStatus = LoadingStatus.loading.obs;//loading status is observable
 
   Future<void> uploadData() async {
+    loadingStatus.value = LoadingStatus.loading;//0
     final fireStore = FirebaseFirestore.instance;
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
-        // ===>> TO LOAD JSON FILES
         .loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    // ===>> TO LOAD JSON FILES
     final paperInAssets = manifestMap.keys
         .where(
           (path) =>
@@ -37,10 +40,26 @@ class DataUploader extends GetxController {
         'title': paper.title,
         'image_url': paper.imageUrl,
         'description': paper.description,
-        'time_seconds':paper.timeSeconds,
-        'questions_count': paper.questions==null ? 0 : paper.questions?.length
+        'time_seconds': paper.timeSeconds,
+        'questions_count': paper.questions == null ? 0 : paper.questions?.length
       });
+      for (Questions questions in paper.questions!) {
+        final questionPath =
+            questionRF(paperId: paper.id, questionId: questions.id);
+        batch.set(questionPath, {
+          'question': questions.question,
+          'correct_answer': questions.correctAnswer,
+        });
+
+        for (var answer in questions.answers) {
+          batch.set(questionPath.collection('answers').doc(answer.identifier), {
+            'identifier': answer.identifier,
+            'answer': answer.answer,
+          });
+        }
+      }
     }
     await batch.commit();
+    loadingStatus.value = LoadingStatus.completed;
   }
 }
--- lib/firebase_ref/references.dart
+++ lib/firebase_ref/references.dart
@@ -2,3 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 final fireStore = FirebaseFirestore.instance;
 final questioPaperRF = fireStore.collection('questionPapers');
+DocumentReference questionRF({
+  required String paperId,
+  required String questionId,
+}) {
+  return questioPaperRF.doc(paperId).collection('questions').doc(questionId);
+}
